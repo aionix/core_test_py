01bd517dad42259416d6b19c96efae0de5f57bb3
Artem <aionix@yandex.ua>
2018-10-30 23:46:38 +0200

    add option - for running from console with options

--- conftest.py
+++ conftest.py
@@ -3,10 +3,35 @@ import pytest
 from fixture.application import Application
 
 
-@pytest.fixture
+fixture = None
+
+@pytest.fixture()
 def app(request):
-    fixture = Application()
-    fixture.session.open_home_page()
-    fixture.session.login(username="admin", password="secret")
-    request.addfinalizer(fixture.destroy)
+    global fixture
+    if fixture is None:
+        browser = request.config.getoption("--browser")
+        base_url = request.config.getoption("--baseUrl")
+        fixture = Application(browser=browser, base_url=base_url)
+
+        fixture.session.open_home_page()
+        fixture.session.login(username="admin", password="secret")
+    else:
+        if not fixture.is_valid():
+            browser = request.config.getoption("--browser")
+            base_url = request.config.getoption("--baseUrl")
+            fixture = Application(browser=browser, base_url=base_url)
+        fixture.session.open_home_page()
+        fixture.session.login(username="admin", password="secret")
     return fixture
+
+@pytest.fixture(autouse=True)
+def stop(request):
+    def fin():
+        if fixture is not None:
+            fixture.finish()
+    request.addfinalizer(fin)
+    return fixture
+
+def pytest_addoption(parser):
+    parser.addoption("--browser", action="store", default="chrome")
+    parser.addoption("--baseUrl", action="store", default="http://localhost:8090/addressbook/group.php")
