1190be0b122bcbc47d4f316959ea0464e3aeed9d
Artem <aionix@yandex.ua>
2019-03-27 01:40:17 +0200

    group object + conftest modifications

--- conftest.py
+++ conftest.py
@@ -1,3 +1,5 @@
+import json
+
 import pytest
 
 from fixture.application import Application
@@ -5,24 +7,19 @@ from fixture.application import Application
 
 fixture = None
 
-@pytest.fixture()
+@pytest.fixture
 def app(request):
     global fixture
-    if fixture is None:
-        browser = request.config.getoption("--browser")
-        base_url = request.config.getoption("--baseUrl")
-        fixture = Application(browser=browser, base_url=base_url)
-
+    browser = request.config.getoption("--browser")
+    with open(request.config.getoption("--target")) as config_file:
+        target = json.load(config_file)
+    base_url = target["baseUrl"]
+    if fixture is None or not fixture.is_valid():
+        fixture = Application(browser=browser, base_url=target['baseUrl'])
         fixture.session.open_home_page()
-        fixture.session.login(username="admin", password="secret")
-    else:
-        if not fixture.is_valid():
-            browser = request.config.getoption("--browser")
-            base_url = request.config.getoption("--baseUrl")
-            fixture = Application(browser=browser, base_url=base_url)
-        fixture.session.open_home_page()
-        fixture.session.login(username="admin", password="secret")
+        fixture.session.login(username=target['username'], password=target['password'])
     return fixture
+
 
 @pytest.fixture(autouse=True)
 def stop(request):
@@ -34,4 +31,8 @@ def stop(request):
 
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="chrome")
-    parser.addoption("--baseUrl", action="store", default="http://localhost:8090/addressbook/group.php")
+    parser.addoption("--target", action="store", default="target.json")
+
+@pytest.fixture()
+def attach_screen(request):
+    yield True
--- model/group.py
+++ model/group.py
@@ -1,7 +1,24 @@
+from sys import maxsize
+
 
 class Group:
-    def __init__(self, name=None, header=None, footer=None):
+    def __init__(self, name=None, header=None, footer=None, id=None):
         self.name = name
         self.header = header
         self.footer = footer
+        self.id = id
+
+    #Same as toString()
+    def __repr__(self):
+        return "name:%s, id:%s, header:%s, footer:%s" % (self.name, self.id, self.header, self.footer)
+
+    #Same as equals()
+    def __eq__(self, other):
+        return (self.id is None or other.id is None or self.name == other.name) #and self.header == other.header
+
+    def sort_by_id(self):
+        if self.id:
+            return int(self.id)
+        else:
+            return maxsize
 
